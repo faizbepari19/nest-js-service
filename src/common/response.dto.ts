86fc2d462031cd6395b1b1dc5124c552f74e3639
faiz-bidchat <faiz.bepari@bidchat.com>
2024-02-18 17:52:56 +0530

    Changed response format to snake_case

--- src/common/response.dto.ts
+++ src/common/response.dto.ts
@@ -1,10 +1,10 @@
 export class ResponseDto {
-    statusCode: number;
+    status_code: number;
     message: string;
     data?: any;
 
     constructor({ statusCode, message, data }: { statusCode: number; message: string; data?: any }) {
-        this.statusCode = statusCode;
+        this.status_code = statusCode;
         this.message = message;
         this.data = data;
     }
